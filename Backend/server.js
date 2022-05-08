87bc3759a4e43561aa36ee5532e252711c2dc467
Dilshan Dissanayake <dilshan@archmage.lk>
2022-05-08 16:37:06 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,24 +20,24 @@ app.use(cors());
 app.use(postRoutes)
 app.use(express.json());
 
-// const uri =process.env.ATLAS_URI;//The location where the database is
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
-// );
-// const connection=mongoose.connection;
-// connection.once('open', ()=>{//Once the connection is open it's gonna open the below log
-//     console.log("MongoDB database connection established successfully");
-// }) 
+const uri =process.env.ATLAS_URI;//The location where the database is
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection=mongoose.connection;
+connection.once('open', ()=>{//Once the connection is open it's gonna open the below log
+    console.log("MongoDB database connection established successfully");
+}) 
 
-// const PORT=8000;
-const DB_URl='mongodb+srv://Dilshan:20Dilshan*@cluster0.4q4dt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(DB_URl)
-.then(()=>{
-    console.log('DB connected')
-})
-.catch((err)=>console.log('connection err',err))
+const PORT=8000;
+// const DB_URl='mongodb+srv://Dilshan:20Dilshan*@cluster0.4q4dt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// mongoose.connect(DB_URl)
+// .then(()=>{
+//     console.log('DB connected')
+// })
+// .catch((err)=>console.log('connection err',err))
 
-app.listen(port, ()=>{
+// app.listen(port, ()=>{
 
-    console.log(`Server is running on port:${port}`);
+//     console.log(`Server is running on port:${port}`);
 
-})
+// })
